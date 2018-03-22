7355b84b60b99a102ca385d69b79ddca91af7e56
Jelle Overbeek <j.overbeek@recognize.nl>
2018-03-22 12:29:29 +0100

    cleaned up code and implemented object literal pattern

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const nunjucks = require('nunjucks')
 const request = require('request')
 const parseJson = require('parse-json')
 
-const sparqlQuery = `
+const api = {
+    streets: [],
+    sparqlQuery : `
             PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
             PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
             PREFIX hg: <http://rdf.histograph.io/>
@@ -20,49 +22,49 @@ const sparqlQuery = `
             }
             
             ORDER BY ?date
-            LIMIT 9999`;
+            LIMIT 9999`,
+    queryUrl: function () {
+        return 'https://api.data.adamlink.nl/datasets/AdamNet/all/services/hva2018/sparql?default-graph-uri=&query=' + encodeURIComponent(this.sparqlQuery) + '&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on'
+    },
+    getData: function () {
+        request(this.queryUrl(), (error, response, data) => {
 
-// The URL
-function queryUrl() {
-    return 'https://api.data.adamlink.nl/datasets/AdamNet/all/services/hva2018/sparql?default-graph-uri=&query=' + encodeURIComponent(sparqlQuery) + '&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on'
+            if(!error) {
+                let latestYear = 0,
+                    latestIndex = -1;
+
+                data = parseJson(data);
+                data.results.bindings.forEach((street) => {
+
+                    const streetObj = {
+                        year: "",
+                        streets: []
+                    }
+
+                    if(street.date.value > latestYear) {
+                        latestYear = street.date.value
+                        latestIndex++
+
+                        // Add data to temporary obj
+                        streetObj.year = street.date.value
+                        streetObj.streets.push(street.label.value)
+
+                        this.streets.push(streetObj);
+                    } else {
+                        // Add street to the existing obj if the year already exists
+                        this.streets[latestIndex].streets.push(street.label.value)
+                    }
+                });
+            } else {
+                console.log('error:', error); // Print the error if one occurred
+            }
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        });
+    },
+    init: function () {
+        this.getData();
+    }
 }
-
-const streets = [];
-
-request(queryUrl(), function (error, response, data) {
-    let latestYear = 0,
-        latestIndex = -1;
-
-    data = parseJson(data);
-
-    data.results.bindings.forEach((street) => {
-
-        const streetObj = {
-            year: "",
-            streets: []
-        }
-
-        if(street.date.value > latestYear) {
-            latestYear = street.date.value
-            latestIndex++
-
-            // Add data to temporary obj
-            streetObj.year = street.date.value
-            streetObj.streets.push(street.label.value)
-
-            streets.push(streetObj);
-        } else {
-            // Add street to the existing obj if the year already exists
-            streets[latestIndex].streets.push(street.label.value)
-        }
-    });
-
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', data);
-
-    console.log(streets);
-});
 
 nunjucks.configure('views', {
     autoescape: true,
@@ -73,10 +75,11 @@ app.use(express.static(__dirname + '/assets'))
 
 app.get('/', function(req, res) {
     res.render('index.html', {
-        streets: streets
+        streets: api.streets
     })
 })
 
 app.listen(8000, function () {
-    console.log('Example app listening on port 8000!')
+    api.init();
+    console.log('Listening on port 8000!')
 })
