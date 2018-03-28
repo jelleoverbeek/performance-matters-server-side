4e7602fa341770b6267f4de8bbbd35010184ceb1
Jelle Overbeek <j.overbeek@recognize.nl>
2018-03-28 22:57:17 +0200

    small fix to gzip all the assets, user will now get message when he/she is offline.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,11 @@ var toggle = require('./toggle')
 
 var app = {
     init: function () {
+
+        if (!navigator.onLine) {
+            document.body.insertAdjacentHTML("afterbegin", '<p class="offline">You appear to be offline, please check your internet connection.</p>')
+        }
+
         toggle.init();
     }
 };
